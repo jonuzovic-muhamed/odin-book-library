560a4a891cbef8b45f0080fc313939d3a3c2b69b
Muhamed Jonuzovic <155450175+jonuzovic-muhamed@users.noreply.github.com>
2024-12-19 17:30:51 +0100

    Add JS function to display the contents of books
    Written a JS function to display the contents of the book array on the
    web page as where each book is a single card in the grid.
    Added so books manually in order to have some data.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const bookLibrary = [];
+const productsSection = document.getElementById('products-section');
+const templateBookCard = document.getElementById('product-item-template');
 
-function Book(title, author, pages, rating) {
+let bookCounter = 0;
+
+function Book(title, author, pages, hasBeenRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.rating = rating;
+  this.hasBeenRead = hasBeenRead;
 }
 
 function addBookToLibrary(title, author, pages, hasBeenRead) {
@@ -14,9 +18,44 @@ function addBookToLibrary(title, author, pages, hasBeenRead) {
 
 function displayLibrary() {
   for (let book of bookLibrary) {
-    // TODO - Implement how to display the book on Web Page
-    book.info();
+    let currentBookCard = templateBookCard.cloneNode(true);
+    currentBookCard.id = `${book.title}-${bookCounter}`
+    currentBookCard.children[0].innerHTML = book.title;
+    currentBookCard.children[1].innerHTML = book.author;
+    currentBookCard.children[2].innerHTML = book.pages;
+    currentBookCard.children[3].innerHTML = book.hasBeenRead ? 'Has been read' : 'Has not been read';
+    productsSection.appendChild(currentBookCard);
+    bookCounter++;
   }
 }
 
-// TODO - Implement button even listener for saving a book by the end-user.
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("Moby Dick", "Herman Melville", 635, false);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, true);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 214, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("War and Peace", "Leo Tolstoy", 1225, false);
+addBookToLibrary("The Odyssey", "Homer", 352, true);
+addBookToLibrary("Brave New World", "Aldous Huxley", 268, false);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 214, false);
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1178, true);
+addBookToLibrary("Crime and Punishment", "Fyodor Dostoevsky", 430, false);
+addBookToLibrary("The Great Expectations", "Charles Dickens", 505, true);
+addBookToLibrary("Anna Karenina", "Leo Tolstoy", 864, true);
+addBookToLibrary("Catch-22", "Joseph Heller", 453, false);
+addBookToLibrary("Frankenstein", "Mary Shelley", 280, true);
+addBookToLibrary("Brave New World", "Aldous Huxley", 311, true);
+addBookToLibrary("The Picture of Dorian Gray", "Oscar Wilde", 254, false);
+addBookToLibrary("The Brothers Karamazov", "Fyodor Dostoevsky", 796, true);
+addBookToLibrary("The Alchemist", "Paulo Coelho", 208, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("The Da Vinci Code", "Dan Brown", 689, true);
+addBookToLibrary("The Shining", "Stephen King", 447, false);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+addBookToLibrary("The Odyssey", "Homer", 352, false);
+addBookToLibrary("The Hunger Games", "Suzanne Collins", 374, true);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 214, true);
+
+displayLibrary();
